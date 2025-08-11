e550029fd6e74b464c3afa2a5c2d954f0c74a809
Endry JR <endryreyes199@gmail.com>
2025-08-11 09:20:58 -0400

    Primer commit

--- netlify/functions/telegram-webhook.js
+++ netlify/functions/telegram-webhook.js
@@ -210,9 +210,9 @@ exports.handler = async function(event, context) {
 *Paquete:* ${escapeMarkdownV2(cleanedPackageName || 'N/A')}
 *Monto:* ${escapeMarkdownV2(transaction.final_price || 'N/A')} ${escapeMarkdownV2(transaction.currency || 'N/A')}
 *Método de Pago:* ${escapeMarkdownV2(transaction.payment_method.replace(/-/g, ' ').toUpperCase() || 'N/A')}
-\\-\\-\\-
+\\-\\-\-
 *Estado:* ${statusText} ${statusEmoji}
-_Marcada por:_ *${escapeMarkdownV2(userName)}* (${escapeMarkdownV2(formattedTime)} ${escapeMarkdownV2(formattedDate)})
+_Marcada por:_ *${escapeMarkdownV2(userName)}* \\(${escapeMarkdownV2(formattedTime)} ${escapeMarkdownV2(formattedDate)}\\)
 `.trim();
 
                 let updatedInlineKeyboard = [];
